3a7503debb97aea65a53c47b87bc7486039b1dfd
Soficius <103771506+Soficius@users.noreply.github.com>
2022-07-07 17:44:43 +0200

    milestone 2

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-// Dato un array di oggetti letterali con:
-// - url dell’immagine
-// - titolo
-// - descrizione
-// Creare un carosello ispirandosi alla foto allegata. Potete anche usare come base il carosello dell'esercizio precedente
-// ## Milstone 0:
-// Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l’immagine grande in modo da poter stilare lo slider.
-// ## Milestone 1:
-// Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
-// Al click dell’utente sulle frecce verso sinistra o destra, l’immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
-// ## Milestone 2:
-// Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l’utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l’ultima e viceversa per l’ultima miniatura se l’utente clicca la freccia verso sinistra.
-
-
 // 1. mi creo il mio array di immagini
 const images = [
     {
@@ -53,34 +39,39 @@ const imgsEls = []
 // 4. ciclo sull'immagini del mio array per creare l'elemento corrispondente
 images.forEach((image,i) => {
 // 4.1 creo elemento div (contenitore) e img (figlio)
-    const div = document.createElement('div') 
+    const cItem = document.createElement('div') 
+    const figure = document.createElement('figure')
+    const text = document.createElement('div')
+    const title = document.createElement('h2')
+    const desc = document.createElement('p')
     const img = document.createElement('img')
 
-    // 4.2 verifico se l'elemento è attivo
-    if(active===i){
+    // 4.2 assegno la classe se l'elemento è attivo 
+    cItem.className = 'carousel-item' + (active===i ? ' active' : '')
 
-        // 4.2.1 se l'elemento è attivo lo segno anche tra le classi 
-        div.className = 'carousel-item active'
-    } else{
+    text.className = 'text'
 
-        // 4.2.2 se l'elemento non è attivo assegno solo la classa base del contenitore
-        div.className = 'carousel-item'
-    }
+    title.innerText = image.title 
+
+    desc.innerText = image.description
 
     // 4.3 assegno le classi all'immagine creata alla riga 16
     img.className = 'd-block w-100'
-
     // 4.4 assegno la sorgente dell'immagine usando quella dell'array
     img.src = image.url
 
     // 4.5 aggiungo img(figlio) al container div
-    div.appendChild(img)
+    text.appendChild(title)
+    text.appendChild(desc)
+    figure.appendChild(text)
+    figure.appendChild(img)
+    cItem.appendChild(figure)
 
     // 4.6 aggiungo all'array degli elementi generati l'elemento generato (div + img)
-    imgsEls[i] = div
+    imgsEls[i] = cItem
 
     // 4.7 aggiungo l'elemento generato in pagina
-    carosello.appendChild(div)
+    carosello.appendChild(cItem)
 })
 
 // 5. mi metto in ascolto del click del btn prev
